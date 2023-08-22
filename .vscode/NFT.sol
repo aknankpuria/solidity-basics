47fdec6c24ca50c1b5342d56e547c05c897e5456
Aslam Khan shah <aksi34012@gmail.com>
2023-08-23 01:55:04 +0530

    nft

--- .vscode/NFT.sol
+++ .vscode/NFT.sol
@@ -1,26 +1,14 @@
-//SPDX_License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-pragma solidity 0.8.8;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract Luffytaro is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Luffytaro", "Luffy") {}
 
-
-
-contract Luffy_NFT is ERC721{
-     uint256 public tokencounter;
-   constructor ( ) public ERC721( "Luffytaro","Luffy"){
-
-       tokencounter = 0;
-
-   }
-
-   function createcollectable(string memory tokenURI) public returns (uint256){
-
-  uint256 newitemid = tokencounter;
-   _safeMint(msg.sender , newitemid);
-   _setTokenURI(newitemid , tokenURI);
-   tokencounter = tokencounter+1;
-   return newitemid;
-   }
-
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
