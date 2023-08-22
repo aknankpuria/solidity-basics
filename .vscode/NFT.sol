bfb49fce21d2b9dedc40c0242f8f5f30124b4239
Aslam Khan shah <aksi34012@gmail.com>
2023-08-22 23:59:49 +0530

    nft

--- .vscode/NFT.sol
+++ .vscode/NFT.sol
@@ -1,3 +1,26 @@
 //SPDX_License-Identifier: MIT
 
 pragma solidity 0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+
+contract Luffy_NFT is ERC721{
+     uint256 public tokencounter;
+   constructor ( ) public ERC721( "Luffytaro","Luffy"){
+
+       tokencounter = 0;
+
+   }
+
+   function createcollectable(string memory tokenURI) public returns (uint256){
+
+  uint256 newitemid = tokencounter;
+   _safeMint(msg.sender , newitemid);
+   _setTokenURI(newitemid , tokenURI);
+   tokencounter = tokencounter+1;
+   return newitemid;
+   }
+
+}
