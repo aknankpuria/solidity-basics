e814c23a86b8dac640e7ca5fbac9beca314cae2e
Aslam Khan shah <aksi34012@gmail.com>
2023-08-22 18:14:49 +0530

    yo

---
+++ storage.sol
@@ -0,0 +1,17 @@
+//SPDX_License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+
+contract SimpleStorage{
+
+
+//boolean , unit , int , address , bytes
+bool luffybool = true;
+uint256 luffyunit = 39;
+string name = "luffy";
+address luffyAddress = 0xf54A000e98d5fe72Fcc8d9524567009066a93FDA;
+bytes32  luffyBytes  = "zoro";  //0x29837894767689697  // random number
+
+
+}
