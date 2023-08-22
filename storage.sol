765ac902b17034b0a632add93e2a5f5bee9190fa
Aslam Khan shah <aksi34012@gmail.com>
2023-08-22 18:30:08 +0530

    yo

--- storage.sol
+++ storage.sol
@@ -8,7 +8,7 @@ contract SimpleStorage{
 
 //boolean , unit , int , address , bytes
 //applybool luffybool = true;
-uint256 luffyunit ; //by default 0
+uint256 public luffyuint ; //by default 0
 // string name = "luffy";
 // address luffyAddress = 0xf54A000e98d5fe72Fcc8d9524567009066a93FDA;
 // bytes32  luffyBytes  = "zoro";  //0x29837894767689697  // random number
@@ -16,9 +16,13 @@ uint256 luffyunit ; //by default 0
 
 //function
 
-function store ( uint256 _luffyunit) public {
+function store ( uint256 _luffyuint) public {
 
-    luffyunit = _luffyunit;
+    luffyuint = _luffyuint;
 }
 
+function get () public view returns (uint256)
+{
+    return luffyuint;
+}
 }
